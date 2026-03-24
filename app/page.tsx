2a100ddf6149f47ec15fb5cfacedd4614e1e2d30
wladj13 <144981392+wladj13@users.noreply.github.com>
2026-03-24 14:07:47 -0600

    Change redirect path in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation'
+  import { redirect } from 'next/navigation'
 
-export default function Home() {
-  redirect('/dashboard')
-}
+  export default function Home() {
+    redirect('/portal/registro')
+  }
